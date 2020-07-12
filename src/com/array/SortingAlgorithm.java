c847c24d79a71e53d90f2ccabc52b5a54e3ec2d4
Abhisek Yadav <abhisek.yadav@sap.com>
2020-07-12 10:24:48 +0530

    bubble insertion sort

--- src/com/array/SortingAlgorithm.java
+++ src/com/array/SortingAlgorithm.java
@@ -21,7 +21,6 @@ public class SortingAlgorithm {
         int i = l;
         int j = l;
 
-
         while (j < pos) {
 
             if (arr[j] < pivot) {
@@ -82,6 +81,30 @@ public class SortingAlgorithm {
     }
 
 
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            int x = arr[i];
+
+            while (j >= 0 && arr[j] > x) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = x;
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
     public static void printArray(int[] arr) {
 
         Arrays.stream(arr).forEach(e -> System.out.print(e + ", "));
@@ -92,8 +115,13 @@ public class SortingAlgorithm {
 
         int[] arr = {8, 9, 7, 6, 2, 4, 5, 10, 1, 3};
 
-        quickSort1(arr, 0, arr.length - 1);
+//        quickSort(arr, 0, arr.length - 1);
 
+//        quickSort1(arr, 0, arr.length - 1);
+
+//        insertionSort(arr);
+
+        bubbleSort(arr);
         printArray(arr);
     }
 
