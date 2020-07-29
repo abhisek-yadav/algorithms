fe25deb9aa51f0fb50b37424125102fa3f25493e
Abhisek Yadav <abhisek.yadav@sap.com>
2020-07-29 07:44:21 +0530

    kth smallest element

--- src/com/array/ArrayOrderStatistics.java
+++ src/com/array/ArrayOrderStatistics.java
@@ -1,6 +1,104 @@
 package com.array;
 
+import java.util.Arrays;
+
 public class ArrayOrderStatistics {
+
+    /**
+     * Find the Kth smallest element in an unsorted array.
+     *
+     * @param arr
+     * @param l
+     * @param h
+     * @param k
+     * @return Kth smallest element
+     */
+    public static int quickSelect(int[] arr, int l, int h, int k) {
+
+        if (k >= l && l <= h) {
+
+            int pivot = pivot(arr, l, h);
+
+            if (k - 1 == pivot)
+                return arr[pivot];
+
+            if (k - 1 < pivot)
+                return quickSelect(arr, l, pivot - 1, k);
+
+            return quickSelect(arr, pivot + 1, h, k);
+        }
+
+        return -1;
+    }
+
+    private static int pivot(int[] arr, int l, int h) {
+
+        int pivot = arr[h];
+        int i = l;
+        int j = l;
+
+        while (j < h) {
+
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                ++i;
+            }
+            ++j;
+        }
+        swap(arr, i, h);
+
+        return i;
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+
+    }
+
+
+    /**
+     * Find k smallest elements in an given unsorted array.
+     * <p>
+     * This algorithm uses Bubble Sort k times.
+     * <p>
+     * time complexity: O(n*k)
+     * <p>
+     * Worst case complexity will be:  O(n*n) when k is equal to the length of an array.
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] kSmallestElements(int[] arr, int k) {
+
+        if (k > arr.length)
+            throw new IllegalArgumentException("k can not be greater than array length");
+
+        if (k == arr.length) {
+            Arrays.sort(arr);
+            return arr;
+        }
+
+        int[] elements = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 1; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1])
+                    swap(arr, j, j + 1);
+            }
+        }
+
+        int e = 0;
+
+        for (int x = arr.length - 1; x >= arr.length - k; x--)
+            elements[e++] = arr[x];
+
+        return elements;
+    }
+
 
     public static void insertionSort(int[] arr) {
 
@@ -15,9 +113,7 @@ public class ArrayOrderStatistics {
             arr[j] = element;
         }
 
-        for (int e : arr) {
-            System.out.print(e + ", ");
-        }
+        Arrays.stream(arr).forEach(e -> System.out.print(e + ", "));
     }
 
     // Kadane's Algorithm
@@ -73,21 +169,31 @@ public class ArrayOrderStatistics {
                 endIndex = i;
             }
         }
-        System.out.println("max sum: " + maxSoFar + ", start index: " + startIndex + ", end index: " + endIndex);
 
+        System.out.println("max sum: " + maxSoFar + ", start index: " + startIndex + ", end index: " + endIndex);
 
     }
 
+    public static void printArray(int[] arr) {
+        Arrays.stream(arr).forEach(e -> System.out.print(e + ", "));
+    }
 
     public static void main(String[] args) {
 
-        int[] arr = new int[]{-5, -3, 4, 1, -2, 1, -5, 3};
+//        int[] arr = new int[]{-5, -3, 4, 1, -2, 1, -5, 3};
 
 //        largestSumContigiousSubArray(arr);
 
 //        largestSumContigiousSubArrayWithNegative(arr);
 
-        insertionSort(arr);
+//        insertionSort(arr);
 
+        int[] arr = {7, 10, 4, 3, 20, 15, 1, 2, 5, 6, 8, 9};
+//        System.out.println(quickSelect(arr, 0, arr.length - 1, 2));
+
+        int[] a = kSmallestElements(arr, 12);
+        printArray(a);
+
+        printArray(arr);
     }
 }
