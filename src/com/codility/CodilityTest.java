35946489810aa4c69975e118a72a4272e1e8c50f
Abhisek Yadav <abhisek.yadav@sap.com>
2020-07-31 08:25:24 +0530

    missing number problem using hash

--- src/com/codility/CodilityTest.java
+++ src/com/codility/CodilityTest.java
@@ -1,6 +1,10 @@
 package com.codility;
 
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CodilityTest {
 
     /**
@@ -40,13 +44,36 @@ public class CodilityTest {
         return result;
     }
 
+    /**
+     * Finding out the smallest missing number starting from 1 from a given array.
+     * <p>
+     * Time Complexity:  O(n)
+     * Space Complexity:  O(n)
+     *
+     * @param A
+     * @return
+     */
+    public static int missingIntegerUsingHash(int[] A) {
+
+        int result = 1;
+        Set<Integer> hash = new HashSet<>();
+
+        Arrays.stream(A).filter(e -> e > 0).forEach(hash::add);
+
+        while (hash.contains(result))
+            ++result;
+
+        return result;
+    }
 
     public static void main(String[] args) {
 
 //        int[] arr = {1, 3, 6, 4, 1, 2};
         int[] arr = {1, 2, 3};
 
-        System.out.println(missingInteger(arr));
+//        System.out.println(missingInteger(arr));
+
+        System.out.println(missingIntegerUsingHash(arr));
 
     }
 }
